17a3a221db4d14875644707dc969d1bb790d4e03
Nirmala Valluri <nirmala.valluri@yahoo.com>
2020-01-16 16:24:17 -0800

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello world'));
-
-app.listen (port, () =>  console.log('SErver is running on port'+ port));
